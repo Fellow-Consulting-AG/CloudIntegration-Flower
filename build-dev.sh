3845ab0c9da04e9153714177298bc5eb04aeee9b
Daniel Jordan <daniel.jordan@fellow-consulting.de>
2021-02-11 16:02:29 +0100

    add broker

--- build-dev.sh
+++
@@ -1,4 +0,0 @@
-docker build -t dajor85570/flower-dev -f Dockerfile_DEV . 
-docker tag dajor85570/flower-dev registry.digitalocean.com/sumup2srvdesk/flower-dev
-docker push registry.digitalocean.com/sumup2srvdesk/flower-dev
-
